217ef32ec7c9d3e35a8ae6a61762be3ca23940a7
Nadia Tabakova <ntabakova@gmail.com>
2022-03-06 23:59:41 +0200

    finalized 2

--- src/requests/requests.js
+++ src/requests/requests.js
@@ -6,7 +6,6 @@ const fetchData = async (url) => {
   // clientId & clientSecret should normally be not exposed here
   const clientId = "37ca67209d5d459091f55a8fcc0f34cc";
   const clientSecret = "77fceae630c547829395eda42975f6e3";
-  let token;
 
   const headers = {
     headers: {
@@ -18,15 +17,13 @@ const fetchData = async (url) => {
     grant_type: "client_credentials",
   };
 
-  try {
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      qs.stringify(data),
-      headers
-    );
+  const authorization = await axios.post(
+    "https://accounts.spotify.com/api/token",
+    qs.stringify(data),
+    headers
+  );
 
-    token = response.data.access_token;
-  } catch (error) {}
+  const token = authorization.data.access_token;
 
   const response = await axios.get(url, {
     headers: {
